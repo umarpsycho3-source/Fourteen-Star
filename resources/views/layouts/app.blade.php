7286e2d6b813e6eb74b8265bbac5c6365222898f
Umar Mohamad <umarxgamer04@gmail.com>
2026-04-15 23:12:18 +0530

    Force HTTPS assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fourteen Star</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
 </head>
 <body>
 
