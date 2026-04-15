2f674e627f53bc222474d164f2fd07c723135a03
Umar Mohamad <umarxgamer04@gmail.com>
2026-04-15 23:50:50 +0530

    Add admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,7 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // User::factory(10)->create();
+    {        $this->call(AdminUserSeeder::class);
 
         User::factory()->create([
             'name' => 'Test User',
